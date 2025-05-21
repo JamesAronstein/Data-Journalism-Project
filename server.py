a60fa30b1b96c685748e827c67cd8117578dd748
JamesAronstein <james.aronstein26@trinityschoolnyc.org>
2025-05-21 13:57:13 -0400

    j

---
+++ server.py
@@ -0,0 +1,14 @@
+#should have 3 routes
+
+#one for about
+
+#one for index, which has macro
+
+#and one for the micro, where I can use variables to pass in which micro
+    #its just one boro.html pg, that generates something different based on which boro is called with render template
+    #use if statement, based on which boro, to load which specific svg
+
+
+#for nav bar, I should have nav bar.html, and dynamically insert it into each one, so if I make changes in the nav bar, I only change it once
+
+#templates folder would have files for: about.html, index.html, micro.html, graph.svg, map.svg (would prob have one for all 5 boros)
